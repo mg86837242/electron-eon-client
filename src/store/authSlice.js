fee60939338579382a7c3fd27bf46bb5386969b5
mg86837242 <mg86837242@protonmail.com>
2024-05-30 16:48:20 +0800

    Removed unused states in store

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,12 +1,7 @@
 const createAuthSlice = (set, get) => ({
   // The initial value empty str will be short-circuited if the `token` state has been updated by any action (e.g., `updateToken`), as the persist middleware first evaluates the state persisted in the `localStorage`
   token: '',
-  authUser: null,
-  isAuthUserLoading: true,
   updateToken: newToken => set({ token: newToken }),
-  updateAuthUser: newAuthUser => set({ authUser: newAuthUser }),
-  updateAuthUserLoading: newIsAuthUserLoading =>
-    set({ isAuthUserLoading: newIsAuthUserLoading }),
 });
 
 export default createAuthSlice;
